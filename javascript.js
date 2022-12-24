5a62fc4e24748dfbb89a22bf6725acc140e56da9
Grace <99232016+gracefry@users.noreply.github.com>
2022-12-24 18:31:12 +1100

    Use array instead of storing in variable

--- javascript.js
+++ javascript.js
@@ -15,8 +15,7 @@ function divide(a, b) {
 }
 
 function reset() {
-    a = "";
-    b = "";
+    number = [];
     operator = "";
     screen.textContent = "";
 }
@@ -36,10 +35,8 @@ function operate(operator, a, b) {
     }
 }
 
-let a = "";
-let b = "";
+let number = [];
 let operator = "";
-
 const numbers = document.querySelectorAll(".num");
 const operators = document.querySelectorAll(".operator");
 const screen = document.querySelector("#screen");
@@ -48,15 +45,17 @@ const allClear = document.querySelector("#AC");
 const clear = document.querySelector("#C");
 
 numbers.forEach(button => button.addEventListener('click', (e) => {
-    screen.textContent += e.target.textContent;
+    if (number.length < 2) {
+        screen.textContent += e.target.textContent;
+    }
 }));
 
 operators.forEach(button => button.addEventListener('click', (e) => {
-    if (a == "") {
-        a = screen.textContent;
-        screen.textContent = "";  
+    if (number.length < 2) {
+        number.push(screen.textContent);
+        operator = e.target.textContent;    
     }
-    operator = e.target.textContent;
+    screen.textContent = "";
 }));
 
 allClear.addEventListener('click', (e) => reset());
@@ -66,13 +65,10 @@ clear.addEventListener('click', (e) => {
 })
 
 equals.addEventListener('click', (e) => {
-    if (b == "") {
-        b = screen.textContent;
-        screen.textContent = "";
+    if (number.length < 2) {
+        number.push(screen.textContent);
     }
-
-    let result = operate(operator, a, b);
+    let result = operate(operator, number[0], number[1]);
     screen.textContent = result;
 
-    clear();
 })
